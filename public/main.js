8f06d461876f6258feeb09fcf4706cf3eedd563f
Harita Balaji <balajih@coventry.ac.uk>
2020-12-02 15:02:18 +0000

    Client-side & Linter
    Added aside tag (used for messages) hiding after 5 seconds
    Fixed linter warning

--- public/main.js
+++ public/main.js
@@ -1,6 +1,13 @@
 
 /* main.js */
 
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener('DOMContentLoaded', () => {
 	console.log('DOMContentLoaded')
+	if(document.querySelector('aside')) {
+		const delay = 5000
+		document.querySelector('aside').hidden = false
+		window.setTimeout( () => {
+			document.querySelector('aside').hidden = true
+		},delay)
+	}
 })
